0cadb6e735b88d79def429f67c6e40b6d7eb0b03
BHarper77 <bradyharper11@googlemail.com>
2024-01-20 17:57:04 +0000

    Added lambda infra code

---
+++ deploy/lambda.tf
@@ -0,0 +1,43 @@
+locals {
+  lambda_zip  = "lambda_package.zip"
+  lambda_name = "klbdesigns-email-service"
+}
+
+resource "aws_lambda_function" "email_service" {
+  filename         = local.lambda_zip
+  function_name    = local.lambda_name
+  role             = aws_iam_role.email_service.arn
+  handler          = "index.handler"
+  source_code_hash = data.archive_file.email_service.output_base64sha256
+  runtime          = "nodejs20.x"
+
+  environment {
+    variables = {
+      apiKey = "bar"
+    }
+  }
+}
+
+resource "aws_iam_role" "email_service" {
+  name               = local.lambda_name
+  assume_role_policy = data.aws_iam_policy_document.email_service.json
+}
+
+data "aws_iam_policy_document" "email_service" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "archive_file" "email_service" {
+  type        = "zip"
+  source_file = "../lambda/index.js"
+  output_path = local.lambda_zip
+}
---
+++ deploy/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.26.0"
+    }
+  }
+  backend "s3" {
+    bucket = "klbdesigns-terraform"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
